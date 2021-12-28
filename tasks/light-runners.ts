f0e08c23f77fb1dc24bf265b956bce9d067229de
Clement Walter <clement.walter@ibm.com>
2021-12-28 10:00:19 +0100

    Update light-runners task with stats

--- tasks/light-runners.ts
+++ tasks/light-runners.ts
@@ -1,7 +1,6 @@
 import { task } from "hardhat/config";
 import fs from "fs";
 import { decode, encode } from "../utils/rle";
-import assert from "assert";
 
 type Traits = Record<string, Record<string, string>>;
 
@@ -13,14 +12,14 @@ task("light-runners", "RLE encode the runners traits to a JSON file")
       fs.readFileSync(input, { encoding: "utf-8" })
     );
     const lightTraits: Traits = JSON.parse(JSON.stringify(traits));
-    let bytesCount = 0;
-    let lightBytesCount = 0;
+    const bytesCount = [];
+    const lightBytesCount = [];
     for (const trait of Object.entries(lightTraits)) {
       for (const design of Object.entries(trait[1])) {
         const originalString = design[1].slice(2);
         const encodedString = encode(originalString);
-        bytesCount += originalString.length / 2;
-        lightBytesCount += encodedString.length / 2;
+        bytesCount.push(originalString.length / 2);
+        lightBytesCount.push(encodedString.length / 2);
 
         if (decode(encodedString) !== originalString) {
           console.log(originalString);
@@ -34,6 +33,27 @@ task("light-runners", "RLE encode the runners traits to a JSON file")
 
     fs.writeFileSync(output, JSON.stringify(traits, null, 2));
 
-    console.log(`Original bytes count: ${bytesCount}`);
-    console.log(`Light bytes count: ${lightBytesCount}`);
+    console.log(`Original bytes count: ${bytesCount.reduce((p, c) => p + c)}`);
+    console.log(
+      `Light bytes count: ${lightBytesCount.reduce((p, c) => p + c)}`
+    );
+    console.log(
+      `Ratio: ${Math.round(
+        (lightBytesCount.reduce((p, c) => p + c) /
+          bytesCount.reduce((p, c) => p + c)) *
+          100
+      )}%`
+    );
+    console.log(
+      `Average light bytes length: ${Math.round(
+        lightBytesCount.reduce((p, c) => p + c) / lightBytesCount.length
+      )} VS ${Math.round(
+        bytesCount.reduce((p, c) => p + c) / bytesCount.length
+      )}`
+    );
+    console.log(
+      `Max light bytes length: ${lightBytesCount.reduce((p, c) =>
+        p > c ? p : c
+      )} VS ${bytesCount.reduce((p, c) => (p > c ? p : c))}`
+    );
   });
