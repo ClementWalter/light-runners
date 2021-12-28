a21657ced1a0a2154ed8008b0bf2ef809288733f
Clement Walter <clement.walter@ibm.com>
2021-12-28 08:46:05 +0100

    Fix ts RLE use uint instead of bytes

--- test/utils/rle.test.ts
+++ test/utils/rle.test.ts
@@ -1,14 +1,39 @@
 // See https://stackoverflow.com/a/61155795/4444546
 import { expect } from "chai";
-import { decode, encode } from "../../utils/rle";
+import { decode, encode, intToHex } from "../../utils/rle";
 
 describe("rle", function () {
   let hexString: string, rleString: string;
   beforeEach(() => {
     hexString =
-      "abab" + "cccccc" + "cd" + "dddd" + "ab" + "bc" + "cc" + "dddd" + "ff";
+      "ab".repeat(2) +
+      "cc".repeat(3) +
+      "cd".repeat(1) +
+      "dd".repeat(2) +
+      "ab".repeat(1) +
+      "bc".repeat(1) +
+      "cc".repeat(1) +
+      "dd".repeat(2) +
+      "ff".repeat(1);
     rleString =
-      "2ab" + "3cc" + "1cd" + "2dd" + "1ab" + "1bc" + "1cc" + "2dd" + "1ff";
+      intToHex(2) +
+      "ab" +
+      intToHex(3) +
+      "cc" +
+      intToHex(1) +
+      "cd" +
+      intToHex(2) +
+      "dd" +
+      intToHex(1) +
+      "ab" +
+      intToHex(1) +
+      "bc" +
+      intToHex(1) +
+      "cc" +
+      intToHex(2) +
+      "dd" +
+      intToHex(1) +
+      "ff";
   });
   describe("encode", () => {
     it("should return encoded hexString", () => {
@@ -28,6 +53,22 @@ describe("rle", function () {
   describe("decode/encode", () => {
     it("should return same rleString", () => {
       expect(encode(decode(rleString))).to.equal(rleString);
+    });
+  });
+  describe("intToHex", () => {
+    it("should write number as hex string", () => {
+      expect(intToHex(0)).to.equal("00");
+      expect(intToHex(2)).to.equal("02");
+      expect(intToHex(10)).to.equal("0a");
+      expect(intToHex(10)).to.equal("0a");
+      expect(intToHex(16)).to.equal("10");
+    });
+    it("should return original number", () => {
+      expect(parseInt(intToHex(0), 16)).to.equal(0);
+      expect(parseInt(intToHex(2), 16)).to.equal(2);
+      expect(parseInt(intToHex(10), 16)).to.equal(10);
+      expect(parseInt(intToHex(10), 16)).to.equal(10);
+      expect(parseInt(intToHex(16), 16)).to.equal(16);
     });
   });
 });
--- utils/rle.ts
+++ utils/rle.ts
@@ -1,9 +1,21 @@
 // See https://deepbsd.github.io/js,programming/2019/06/22/Run_Length_Encoding_in_Javascript.html
 
 export const decode = (str: string): string => {
-  return str.replace(/(\d+)(\w{2})/g, (_, count, chr) => chr.repeat(count));
+  return str.replace(/(\w{2})(\w{2})/g, (_, count, chr) =>
+    chr.repeat(parseInt(count, 16))
+  );
 };
 
 export const encode = (str: string): string => {
-  return str.replace(/(\w{2})\1*/g, (group, chr) => group.length / 2 + chr);
+  return str.replace(
+    /(\w{2})\1*/g,
+    (group, chr) => intToHex(group.length / 2) + chr
+  );
+};
+
+// See https://stackoverflow.com/a/34356351/4444546
+// Convert a byte to a hex string
+export const intToHex = (byte: number): string => {
+  const current = byte < 0 ? byte + 256 : byte;
+  return (current >>> 4).toString(16) + (current & 0xf).toString(16);
 };
